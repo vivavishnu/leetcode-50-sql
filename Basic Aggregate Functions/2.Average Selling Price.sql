879a169f4ac8e32b95b7e15076f3189b03571397
VISHNUVARDHAN PILLI <67045601+vivavishnu@users.noreply.github.com>
2024-09-21 21:11:56 +0530

    Update 2.Average Selling Price.sql

--- Basic Aggregate Functions/2.Average Selling Price.sql
+++ Basic Aggregate Functions/2.Average Selling Price.sql
@@ -1,4 +1,5 @@
-select t1.product_id,round(sum(t1.price*t2.units)/sum(t2.units),2) as "average_price" from Prices t1
-join UnitsSold t2
+select t1.product_id,IFNULL(round(sum(t1.price*t2.units)/sum(t2.units),2) , 0) as "average_price" from Prices t1
+left join UnitsSold t2
 on (t2.purchase_date between t1.start_date and t1.end_date) and t1.product_id = t2.product_id 
 group by t1.product_id 
+
