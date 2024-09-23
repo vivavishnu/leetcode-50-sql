f9812e7dfad7d039024f7817df3de7c6697c37f7
VISHNUVARDHAN PILLI <67045601+vivavishnu@users.noreply.github.com>
2024-09-23 21:16:03 +0530

    Create 2.Primary Department for Each Employee.sql

---
+++ Advanced Select and Joins/2.Primary Department for Each Employee.sql
@@ -0,0 +1,10 @@
+select DISTINCT employee_id, department_id
+FROM Employee 
+WHERE employee_id IN
+    (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY 1
+    HAVING COUNT(*) = 1
+    )
+    OR primary_flag = 'Y'
